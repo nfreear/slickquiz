fca6ab20cc3ee82afe10129eb714cf564e6a3d38
jewlofthelotus <jewlofthelotus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-08 19:46:34 +0000

    tagging version 1.1.2 - resolved issue with quizzes not working when they are the only thing in the post / page
    git-svn-id: https://plugins.svn.wordpress.org/slickquiz/tags/1.1.2@649866 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/slickquiz-front.php
+++ php/slickquiz-front.php
@@ -11,6 +11,7 @@ if ( !class_exists( 'SlickQuizFront' ) ) {
 
         var $quiz = null;
         var $status = null;
+        var $pageQuizzes = array();
 
 
         // Constructor
@@ -27,7 +28,7 @@ if ( !class_exists( 'SlickQuizFront' ) ) {
             add_filter( 'the_content', array( &$this, 'load_resources' ) );
             add_filter( 'widget_text', array( &$this, 'load_resources' ) );
 
-            // Make sure dynamic quiz script gets loaded below jQuery
+            // Make sure dynamic quiz scripts gets loaded below jQuery
             add_filter( 'wp_footer', array( &$this, 'load_quiz_script' ), 5000 );
         }
 
@@ -35,10 +36,10 @@ if ( !class_exists( 'SlickQuizFront' ) ) {
         function load_resources( $content )
         {
             // Only load resources when a shortcode is on the page
-            preg_match( '/\[slickquiz[^\]]*\]/is', $content, $matches );
-            if ( count( $matches) == 0 ) return $content;
+            if ( strpos( $content, '[slickquiz' ) === false ) {
+                return $content;
+            }
 
-            // $mainPluginFile = substr( __DIR__, 0, -strlen( basename( __DIR__ ) ) ) . 'slickquiz.php';
             $mainPluginFile = dirname(dirname(__FILE__)) . '/slickquiz.php';
 
             // Scripts
@@ -54,25 +55,91 @@ if ( !class_exists( 'SlickQuizFront' ) ) {
 
         function load_quiz_script()
         {
-            global $quiz, $status;
+            global $pageQuizzes;
 
             $out = '';
 
-            if ( $quiz ) {
-                if ( $status && $status != self::NOT_PUBLISHED ) {
-                    $out = '
-                        <script type="text/javascript">
-                            jQuery(document).ready(function($) {
-                                $("#slickQuiz' . $quiz->id . '").slickQuiz({
-                                    json:             ' . $quiz->publishedJson . ',
-                                    checkAnswerText:  "' . $this->get_admin_option( 'check_answer_text' ) . '",
-                                    nextQuestionText: "' . $this->get_admin_option( 'next_question_text' ) . '",
-                                    backButtonText:   "' . $this->get_admin_option( 'back_button_text' ) . '",
-                                    randomSort:       ' . ( $this->get_admin_option( 'random_sort' ) == '1' ? 'true' : 'false' ) . '
+            if ( count( $pageQuizzes ) ) {
+
+                foreach ( $pageQuizzes as $id => $quizStat ) {
+                    $quiz   = $quizStat[0];
+                    $status = $quizStat[1];
+
+                    if ( $status && $status != self::NOT_PUBLISHED ) {
+                        $out .='
+                            <script type="text/javascript">
+                                jQuery(document).ready(function($) {';
+
+                        $out .= '
+                                    $("#slickQuiz' . $quiz->id . '").slickQuiz({
+                                        json:                        ' . $quiz->publishedJson . ',
+                                        checkAnswerText:             "' . $this->get_admin_option( 'check_answer_text' ) . '",
+                                        nextQuestionText:            "' . $this->get_admin_option( 'next_question_text' ) . '",
+                                        backButtonText:              "' . $this->get_admin_option( 'back_button_text' ) . '",
+                                        randomSortQuestions:         ' . ( $this->get_admin_option( 'random_sort_questions' ) == '1' ? 'true' : 'false' ) . ',
+                                        randomSortAnswers:           ' . ( $this->get_admin_option( 'random_sort_answers' ) == '1' ? 'true' : 'false' ) . ',
+                                        randomSort:                  ' . ( $this->get_admin_option( 'random_sort' ) == '1' ? 'true' : 'false' ) . ',
+                                        preventUnanswered:           ' . ( $this->get_admin_option( 'disable_next' ) == '1' ? 'true' : 'false' ) . ',
+                                        disableResponseMessaging:    ' . ( $this->get_admin_option( 'disable_responses' ) == '1' ? 'true' : 'false' ) . ',
+                                        completionResponseMessaging: ' . ( $this->get_admin_option( 'completion_responses' ) == '1' ? 'true' : 'false' ) . '
+                                    });';
+
+                        if ( $this->get_admin_option( 'save_scores' ) == '1') {
+                            $out .= '
+                                    // get the start button
+                                    var button' . $quiz->id . ' = $("#slickQuiz' . $quiz->id . ' .buttonWrapper a");
+
+                                    // disable the start button
+                                    button' . $quiz->id . '.removeClass("startQuiz").addClass("disabled");
+
+                                    // insert a name field before the button
+                                    $("#slickQuiz' . $quiz->id . ' .buttonWrapper").before(
+                                        "<div class=\"nameLabel\"><label>' . $this->get_admin_option( 'name_label' ) . '</label> <input type=\"text\" /></div>"
+                                    );
+
+                                    // when name is entered, enable start button
+                                    $("#slickQuiz' . $quiz->id . ' .nameLabel input").on("change keyup", function() {
+                                        if ($(this).val() !== "") {
+                                            button' . $quiz->id . '.addClass("startQuiz").removeClass("disabled");
+                                        } else {
+                                            button' . $quiz->id . '.removeClass("startQuiz").addClass("disabled");
+                                        }
+                                    });
+
+                                    // when starting quiz, hide name field
+                                    $("#slickQuiz' . $quiz->id . ' .button.startQuiz").live("click", function() {
+                                        if ($(this).hasClass("disabled") === false) {
+                                            $("#slickQuiz' . $quiz->id . ' .nameLabel").hide();
+                                        }
+                                    });
+
+                                    // watch final check answer button and wait for result calculation before submitting
+                                    $("#slickQuiz' . $quiz->id . ' .button.checkAnswer").last().on("click", function() {
+                                        setTimeout(submitScore' . $quiz->id . ', 2000);
+                                    });
+
+                                    // submit scores to wordpress db
+                                    function submitScore' . $quiz->id . '() {
+                                        var json = {
+                                            name: $("#slickQuiz' . $quiz->id . ' .nameLabel input").val(),
+                                            score: $("#slickQuiz' . $quiz->id . ' .correctResponse").length + " / " + $("#slickQuiz' . $quiz->id . ' .question").length,
+                                            quiz_id: ' . $quiz->id . '
+                                        };
+
+                                        $.ajax({
+                                            type: "POST",
+                                            url: "' . esc_url( wp_nonce_url( site_url( "wp-admin/admin-ajax.php" ), "wp-admin/admin-ajax.php" ) ) . '",
+                                            data: {action: "save_quiz_score", json: JSON.stringify(json)}
+                                        });
+                                    }';
+                        }
+
+                        $out .= '
                                 });
-                            });
-                        </script>';
+                            </script>';
+                    }
                 }
+
             }
 
             echo $out;
@@ -91,12 +158,14 @@ if ( !class_exists( 'SlickQuizFront' ) ) {
 
         function show_slickquiz( $id )
         {
-            global $quiz, $status;
+            global $quiz, $status, $pageQuizzes;
 
             $quiz = $this->get_quiz_by_id( $id );
 
             if ( $quiz ) {
                 $status = $this->get_quiz_status( $quiz );
+
+                $pageQuizzes[$id] = array( $quiz, $status );
 
                 if ( $status == self::NOT_PUBLISHED ) {
                     $out = "<p class='quiz-$id notPublished'>" . $this->get_admin_option( 'disabled_quiz_message' ) . "</p>";
